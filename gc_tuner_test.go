2dada06ccee583dd18efa53c8f8429242daa3d1d
chaocai2001 <chaocai2001@icloud.com>
2022-03-13 16:22:00 +0800

    updated test

--- gc_tuner_test.go
+++ gc_tuner_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func alloc() *[]int64 {
-	b := make([]int64, 500000000)
+	b := make([]int64, 100000000)
 	time.Sleep(time.Millisecond * 1)
 	return &b
 }
@@ -17,16 +17,16 @@ func alloc() *[]int64 {
 var ballast []byte
 
 func TestTuner(t *testing.T) {
-	bb := make([]*[]int64, 10)
+	//bb := make([]*[]int64, 10)
 	NewTuner(false, TuningParam{
 		LowestGOGC:                             100,
-		HighestGOGC:                            50000,
+		HighestGOGC:                            1000,
 		PropertionActiveHeapSizeInTotalMemSize: float64(1),
 		IsToOutputDebugInfo:                    true, //set it false, when running in prod
 	})
 	//debug.SetGCPercent(200)
 	for {
-		bb = append(bb, alloc())
+		// bb = append(bb, alloc())
 		_ = alloc()
 	}
 }
